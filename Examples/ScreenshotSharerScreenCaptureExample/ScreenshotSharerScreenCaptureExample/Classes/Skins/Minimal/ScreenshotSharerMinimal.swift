b832862999e7399e4f5f192d4a567a7f2345801f
Yagiz Gurgul <yagiz@kukaapps.com>
2017-07-14 11:24:11 +0300

    source is also hard added into examples

---
+++ Examples/ScreenshotSharerScreenCaptureExample/ScreenshotSharerScreenCaptureExample/Classes/Skins/Minimal/ScreenshotSharerMinimal.swift
@@ -0,0 +1,148 @@
+/*
+ MIT License
+ 
+ Copyright (c) 2017 Yagiz Gurgul
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import UIKit
+
+open class ScreenshotSharerMinimal: ScreenshotSharerViewController {
+    
+    @IBOutlet weak var screenshotImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    @IBOutlet weak var imageViewRatioConstraint: NSLayoutConstraint!
+    
+    override open func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    override open func setScreenshotImage(_ image: UIImage)
+    {
+        self.screenshotImageView.image = image
+        
+
+        let imageRatioNewConstraint = NSLayoutConstraint(item: self.imageViewRatioConstraint.firstItem,
+                                                           attribute: self.imageViewRatioConstraint.firstAttribute,
+                                                           relatedBy: self.imageViewRatioConstraint.relation,
+                                                           toItem: self.imageViewRatioConstraint.secondItem,
+                                                           attribute: self.imageViewRatioConstraint.secondAttribute,
+                                                           multiplier: image.size.width / image.size.height,
+                                                           constant: self.imageViewRatioConstraint.constant)
+        
+        self.screenshotImageView.removeConstraint(self.imageViewRatioConstraint)
+        self.screenshotImageView.addConstraint(imageRatioNewConstraint)
+        self.imageViewRatioConstraint = imageRatioNewConstraint
+        self.view.layoutIfNeeded()
+    }
+    
+    
+    
+    
+    
+    override open func setShareTitleText(_ text:String)
+    {
+        self.titleLabel.text = text
+    }
+    
+    override open func setShareDescriptionText(_ text:String)
+    {
+        self.descriptionLabel.text = text
+    }
+    
+    override open func setShareButtonTitleText(_ text:String)
+    {
+        self.shareButton.setTitle(text, for: .normal)
+    }
+    
+    
+    
+    
+    
+    override open func setShareTitleFont(_ font: UIFont)
+    {
+        self.titleLabel.font = font
+    }
+    
+    override open func setShareDescriptionFont(_ font: UIFont)
+    {
+        self.descriptionLabel.font = font
+    }
+    
+    override open func setShareButtonTitleFont(_ font: UIFont)
+    {
+        self.shareButton.titleLabel?.font = font
+    }
+    
+    
+    
+    
+    
+    override open func setShareTitleTextColor(_ color: UIColor)
+    {
+        self.titleLabel.textColor = color
+    }
+    
+    override open func setShareDescriptionTextColor(_ color: UIColor)
+    {
+        self.descriptionLabel.textColor = color
+    }
+    
+    override open func setShareButtonTitleColor(_ color: UIColor)
+    {
+        self.shareButton.setTitleColor(color, for: .normal)
+    }
+    
+    override open func setShareButtonBackgroundColor(_ color: UIColor)
+    {
+        self.shareButton.backgroundColor = color
+    }
+    
+    
+    
+    
+    @IBAction func shareButtonAction(_ sender: Any)
+    {
+        let activityItem: [AnyObject] = [self.screenshotImageView.image as AnyObject]
+        let activityViewController = UIActivityViewController(activityItems: activityItem as [AnyObject], applicationActivities: nil)
+        
+        activityViewController.popoverPresentationController?.sourceView = self.shareButton
+        activityViewController.popoverPresentationController?.sourceRect = self.shareButton.bounds
+        
+        self.present(activityViewController, animated: true, completion: nil)
+        
+        activityViewController.completionWithItemsHandler = { activity, success, items, error in
+            
+            if success == true && error == nil
+            {
+                self.screenshotSharer().dismissSharerViewController(true)
+            }
+        }
+    }
+    
+    @IBAction func dismissButtonAction(_ sender: Any)
+    {
+        self.screenshotSharer().dismissSharerViewController(false)
+    }
+}
---
+++ Examples/ScreenshotSharerViewCaptureExample/ScreenshotSharerViewCaptureExample/Classes/ScreenshotSharer.swift
@@ -0,0 +1,215 @@
+/*
+ MIT License
+ 
+ Copyright (c) 2017 Yagiz Gurgul
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import UIKit
+
+open class ScreenshotSharer: NSObject
+{
+    open var sharerViewController:ScreenshotSharerViewController?
+    
+    open weak var capturedView:UIView?
+    open var isCropStatusBar:Bool = false
+    open var cropRect:CGRect!
+    
+    open var captureBlock:((UIImage?,ScreenshotSharerViewController?) -> ())?
+    open var shareCompletionBlock:((Bool) -> ())?
+    
+    open var isEnabled:Bool = false
+    open var isSharerPresented:Bool = false
+    open var isRegistered:Bool = false
+    
+    override public init()
+    {
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.applicationUserDidTakeScreenshot(notification:)), name: NSNotification.Name.UIApplicationUserDidTakeScreenshot, object: nil)
+    }
+    
+    open func registerViewCapturer(view:UIView, cropRect:CGRect, captureBlock:@escaping ((UIImage?,ScreenshotSharerViewController?) -> ()), shareCompletionBlock:@escaping ((Bool) -> ()))
+    {
+        self.registerViewCapturer(view: view, cropRect: cropRect, sharerViewController: ScreenshotSharerMinimal(nibName: "ScreenshotSharerMinimal", bundle: Bundle(for: ScreenshotSharerMinimal.self)), captureBlock: captureBlock, shareCompletionBlock: shareCompletionBlock)
+    }
+    
+    open func registerScreenCapturer(cropStatusBar:Bool, cropRect:CGRect, captureBlock:@escaping ((UIImage?,ScreenshotSharerViewController?) -> ()), shareCompletionBlock:@escaping ((Bool) -> ()))
+    {
+        self.registerScreenCapturer(cropStatusBar: cropStatusBar, cropRect: cropRect, sharerViewController: ScreenshotSharerMinimal(nibName: "ScreenshotSharerMinimal", bundle: Bundle(for: ScreenshotSharerMinimal.self)), captureBlock: captureBlock, shareCompletionBlock: shareCompletionBlock)
+    }
+    
+    open func registerViewCapturer(view:UIView, cropRect:CGRect, sharerViewController:ScreenshotSharerViewController?, captureBlock:@escaping ((UIImage?,ScreenshotSharerViewController?) -> ()), shareCompletionBlock:@escaping ((Bool) -> ()))
+    {
+        if self.isRegistered == true
+        {
+            return
+        }
+        
+        self.capturedView = view
+        self.cropRect = cropRect
+        self.sharerViewController = sharerViewController
+        self.captureBlock = captureBlock
+        self.shareCompletionBlock = shareCompletionBlock
+        
+        self.isRegistered = true
+        self.isEnabled = true
+    }
+    
+    open func registerScreenCapturer(cropStatusBar:Bool, cropRect:CGRect, sharerViewController:ScreenshotSharerViewController?, captureBlock:@escaping ((UIImage?,ScreenshotSharerViewController?) -> ()), shareCompletionBlock:@escaping ((Bool) -> ()))
+    {
+        if self.isRegistered == true
+        {
+            return
+        }
+        
+        self.isCropStatusBar = cropStatusBar
+        self.cropRect = cropRect
+        self.sharerViewController = sharerViewController
+        self.captureBlock = captureBlock
+        self.shareCompletionBlock = shareCompletionBlock
+        
+        self.capturedView = nil
+        
+        self.isRegistered = true
+        self.isEnabled = true
+    }
+    
+    open func unregister()
+    {
+        self.cropRect = nil
+        self.sharerViewController = nil
+        self.captureBlock = nil
+        self.capturedView = nil
+        
+        self.isEnabled = false
+        self.isRegistered = false
+    }
+    
+    
+    func applicationUserDidTakeScreenshot(notification:NSNotification)
+    {
+        let sender = ScreenshotSharerUtility.findTopMostViewController()
+        
+        if self.isEnabled == false || self.isSharerPresented == true || self.isRegistered == false
+        {
+            return
+        }
+        
+        if let view = capturedView, let sender = sender, let sharerViewController = sharerViewController
+        {
+            
+            let image = self.takeImageOf(view: view)
+            let croppedImage = self.crop(image: image, rect: self.cropRect)
+            
+            sender.present(sharerViewController, animated: false, completion: nil)
+            sharerViewController.setScreenshotSharer(self)
+            sharerViewController.setScreenshotImage(croppedImage)
+            
+            self.isSharerPresented = true
+            
+            self.captureBlock?(croppedImage,sharerViewController)
+            
+            return
+            
+        }else if let window = ScreenshotSharerUtility.findTopMostWindow(), let sender = sender, let sharerViewController = sharerViewController
+        {
+            
+            let image = self.takeImageOf(view: window)
+            
+            var croppedImage = image
+            
+            if self.isCropStatusBar == true
+            {
+                croppedImage = self.cropStatusBar(image: image)
+                croppedImage = self.crop(image: croppedImage, rect: self.cropRect)
+                
+            }else
+            {
+                croppedImage = self.crop(image: croppedImage, rect: self.cropRect)
+            }
+            
+            sender.present(sharerViewController, animated: false, completion: nil)
+            sharerViewController.setScreenshotSharer(self)
+            sharerViewController.setScreenshotImage(croppedImage)
+            
+            self.isSharerPresented = true
+            
+            self.captureBlock?(croppedImage,sharerViewController)
+            
+            return
+        }
+        
+        self.captureBlock?(nil,nil)
+    }
+    
+    open func takeImageOf(view:UIView) -> UIImage
+    {
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.isOpaque, 0.0);
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext();
+        
+        return image!
+    }
+    
+    open func cropStatusBar(image:UIImage) -> UIImage
+    {
+        let statusBarHeight = UIApplication.shared.statusBarFrame.size.height
+        return self.crop(image: image, rect: CGRect(x: 0, y: statusBarHeight, width: image.size.width, height: image.size.height - statusBarHeight))
+    }
+    
+    open func crop(image:UIImage,rect:CGRect) -> UIImage
+    {
+        if rect == CGRect.zero
+        {
+            return image
+        }
+        
+        var rect = rect
+        rect.origin.x *= image.scale
+        rect.origin.y *= image.scale
+        rect.size.width *= image.scale
+        rect.size.height *= image.scale
+        
+        let imageRef = image.cgImage!.cropping(to: rect)
+        let image = UIImage(cgImage: imageRef!, scale: image.scale, orientation: image.imageOrientation)
+        
+        return image
+    }
+    
+    open func dismissSharerViewController(_ isSuccess:Bool)
+    {
+        sharerViewController?.dismiss(animated: true, completion: {
+            
+            self.isSharerPresented = false
+            
+            if let shareCompletionBlock = self.shareCompletionBlock
+            {
+                shareCompletionBlock(isSuccess)
+            }
+        })
+    }
+    
+    deinit
+    {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
